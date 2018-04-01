041899ea42019e94ce9a4547f732055c6aa17997
vasco <vastur@gmail.com>
2018-04-01 03:57:03 -0700

    Add package bundler

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonJs from 'rollup-plugin-commonjs';
+import postCss from 'rollup-plugin-postcss';
+import postCssSimpleVars from 'postcss-simple-vars';
+import postCssNested from 'postcss-nested';
+import babel from 'rollup-plugin-babel';
+import { name, homepage, version } from './package.json';
+
+export default {
+  external: ['three'],
+  input: 'src/index.js',
+  output: [
+    {
+      format: 'umd',
+      name: 'ThreeRenderObjects',
+      globals: { three: 'THREE' },
+      file: `dist/${name}.js`,
+      sourcemap: true,
+      banner: `// Version ${version} ${name} - ${homepage}`
+    }
+  ],
+  plugins: [
+    postCss({
+      plugins: [
+        postCssSimpleVars(),
+        postCssNested()
+      ]
+    }),
+    resolve(),
+    commonJs(),
+    babel({ exclude: 'node_modules/**' })
+  ]
+};
