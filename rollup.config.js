9b8dcf9296c4feacd87c28af08247a0c4d261ef0
vasco <vastur@gmail.com>
2020-03-17 23:14:08 -0700

    Add TS declaration

--- rollup.config.js
+++ rollup.config.js
@@ -4,30 +4,76 @@ import postCss from 'rollup-plugin-postcss';
 import postCssSimpleVars from 'postcss-simple-vars';
 import postCssNested from 'postcss-nested';
 import babel from 'rollup-plugin-babel';
-import { name, homepage, version } from './package.json';
+import { terser } from "rollup-plugin-terser";
+import dts from 'rollup-plugin-dts';
+import { name, homepage, version, dependencies, peerDependencies } from './package.json';
 
-export default {
-  external: ['three'],
-  input: 'src/index.js',
-  output: [
-    {
-      format: 'umd',
-      name: 'ThreeRenderObjects',
-      globals: { three: 'THREE' },
-      file: `dist/${name}.js`,
-      sourcemap: true,
-      banner: `// Version ${version} ${name} - ${homepage}`
-    }
-  ],
-  plugins: [
-    postCss({
-      plugins: [
-        postCssSimpleVars(),
-        postCssNested()
-      ]
-    }),
-    resolve(),
-    commonJs(),
-    babel({ exclude: 'node_modules/**' })
-  ]
+const umdConf = {
+  format: 'umd',
+  name: 'ThreeRenderObjects',
+  globals: { three: 'THREE' },
+  banner: `// Version ${version} ${name} - ${homepage}`
 };
+
+export default [
+  {
+    external: ['three'],
+    input: 'src/index.js',
+    output: [
+      { // umd
+        ...umdConf,
+        file: `dist/${name}.js`,
+        sourcemap: true,
+      },
+      { // minify
+        ...umdConf,
+        file: `dist/${name}.min.js`,
+        plugins: [terser({
+          output: { comments: '/Version/' }
+        })]
+      }
+    ],
+    plugins: [
+      postCss({
+        plugins: [
+          postCssSimpleVars(),
+          postCssNested()
+        ]
+      }),
+      resolve(),
+      commonJs(),
+      babel({ exclude: 'node_modules/**' })
+    ]
+  },
+  { // commonJs and ES modules
+    input: 'src/index.js',
+    output: [
+      {
+        format: 'cjs',
+        file: `dist/${name}.common.js`
+      },
+      {
+        format: 'es',
+        file: `dist/${name}.module.js`
+      }
+    ],
+    external: [...Object.keys(dependencies || {}), ...Object.keys(peerDependencies || {})],
+    plugins: [
+      postCss({
+        plugins: [
+          postCssSimpleVars(),
+          postCssNested()
+        ]
+      }),
+      babel()
+    ]
+  },
+  { // expose TS declarations
+    input: 'src/index.d.ts',
+    output: [{
+      file: `dist/${name}.d.ts`,
+      format: 'es'
+    }],
+    plugins: [dts()]
+  }
+];
---
+++ src/index.d.ts
@@ -0,0 +1,75 @@
+import { Object3D, WebGLRendererParameters, Scene, Camera, WebGLRenderer } from 'three';
+
+export interface ConfigOptions {
+  controlType?: 'trackball' | 'orbit' | 'fly';
+  rendererConfig?: WebGLRendererParameters;
+  waitForLoadComplete?: boolean;
+}
+
+interface EffectComposer {
+  // simplified version of
+  render(): void;
+}
+
+type Accessor<In, Out> = Out | string | ((obj: In) => Out);
+type Obj3DAccessor<T> = Accessor<Object3D, T>;
+
+type Coords = { x: number; y: number; z: number; };
+
+type Obj3DCompFn = (a: Object3D, b: Object3D) => number;
+
+export interface ThreeRenderObjectsGenericInstance<ChainableInstance> {
+  (element: HTMLElement): ChainableInstance;
+  resetProps(): ChainableInstance;
+
+  // Data input
+  objects(): Object3D[];
+  objects(objs: Object3D[]): ChainableInstance;
+
+  // Container layout
+  width(): number;
+  width(width: number): ChainableInstance;
+  height(): number;
+  height(height: number): ChainableInstance;
+  skyRadius(): number;
+  skyRadius(glUnits: number): ChainableInstance;
+  backgroundColor(): string;
+  backgroundColor(color: string): ChainableInstance;
+  backgroundImageUrl(): string | null;
+  backgroundImageUrl(url: string | null): ChainableInstance;
+  onBackgroundImageLoaded(callback: () => void): ChainableInstance;
+  showNavInfo(): boolean;
+  showNavInfo(enabled: boolean): ChainableInstance;
+
+  // Render control
+  tick(): ChainableInstance;
+  cameraPosition(): Coords;
+  cameraPosition(position: Partial<Coords>, lookAt?: Coords, transitionMs?: number): ChainableInstance;
+  postProcessingComposer(): EffectComposer;
+  postProcessingComposer(composer: EffectComposer): ChainableInstance;
+  renderer(): WebGLRenderer;
+  scene(): Scene;
+  camera(): Camera;
+  controls(): object;
+
+  // Interaction
+  onClick(callback: (obj: object | null, event: MouseEvent) => void): ChainableInstance;
+  onRightClick(callback: (obj: object | null, event: MouseEvent) => void): ChainableInstance;
+  onHover(callback: (obj: object | null, previousObj: object | null) => void): ChainableInstance;
+  hoverOrderComparator(): Obj3DCompFn;
+  hoverOrderComparator(compFn: Obj3DCompFn): ChainableInstance;
+  lineHoverPrecision(): number;
+  lineHoverPrecision(precision: number): ChainableInstance;
+  tooltipContent(): Obj3DAccessor<string>;
+  tooltipContent(contentAccessor: Obj3DAccessor<string>): ChainableInstance;
+  enablePointerInteraction(): boolean;
+  enablePointerInteraction(enable: boolean): ChainableInstance;
+  hoverDuringDrag(): boolean;
+  hoverDuringDrag(enabled: boolean): ChainableInstance;
+}
+
+export type ThreeRenderObjectsInstance = ThreeRenderObjectsGenericInstance<ThreeRenderObjectsInstance>;
+
+declare function ThreeRenderObjects(configOptions?: ConfigOptions): ThreeRenderObjectsInstance;
+
+export default ThreeRenderObjects;
